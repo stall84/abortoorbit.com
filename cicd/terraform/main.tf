4197f789857257e4b0ec9736dd5cb00bda25f0ee
Michael Stallings <stall84@gmail.com>
2023-07-01 23:34:43 -0400

    Just partially through writing main.tf

--- cicd/terraform/main.tf
+++ cicd/terraform/main.tf
@@ -9,10 +9,22 @@ terraform {
 }
 
 provider "aws" {
-
-  region = "us-east-1"
+  profile = "ato-tf"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "ato-web-bucket" {
+  bucket = "ato-web-bucket-aborttoorbit.com"
+  acl    = "public-read"
+  tags = {
+    Name        = "ato-web-bucket"
+    Environment = "Production"
+  }
 
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "404.html"
+  }
 }
