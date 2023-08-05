985114c46ebd1adac23becf464a613f3e98d1775
Michael Stallings <stall84@gmail.com>
2023-08-05 18:43:49 -0400

    Added new favicon. deleted old non-used images and renamed the head title for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,4 +26,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Abort To Orbit</title>
