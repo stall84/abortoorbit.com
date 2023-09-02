1c3612d40b3d1372cb80b5353712dae2804de09e
Michael Stallings <stall84@gmail.com>
2023-09-02 14:35:23 -0400

    Modified startup script to handle installs.. Added a test condition to build-hook controller to test running commands via webhook.

--- cicd/terraform/scripts/main-startup.sh
+++ cicd/terraform/scripts/main-startup.sh
@@ -24,4 +24,26 @@ echo "---------- HTTPD/APACHE CONFIG COMPLETE ----------"
 export INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 echo "<html><body><h1>Hello from Build Server</h1><p>Instance ID: $INSTANCE_ID</p></body></html>" > /var/www/html/index.html
 
-echo "---------- END MAIN-STARTUP.SH ----------"
+echo "---------- END MAIN-STARTUP SECTION ----------"
+echo "---------- BEGGINING AFTER STARTUP JOBS ----------"
+
+echo "---------- CLOUD-INIT-WAIT ----------"
+cloud-init status --wait
+
+echo "---------- GIT CLONE OF ATO REPO ----------"
+
+cd ~
+git clone https://github.com/stall84/abortoorbit.com.git
+
+echo "---------- NPM INSTALL ATO ----------"
+cd ~/abortoorbit.com && npm install
+
+echo "---------- NPM INSTALL BUILDSERVER ----------"
+cd ~/abortoorbit.com/cicd/buildserver && npm install
+cd ~/abortoorbit.com/cicd/buildserver/scripts && chmod u+x build-script.sh
+
+echo "---------- START SERVER ----------"
+
+cd ~/abortoorbit.com/cicd/buildserver && npm run start
+
+echo "---------- SERVER STARTED ----------"
