eae396cc3787fd73ae621aede652df34059b7d8f
Michael Stallings <stall84@gmail.com>
2023-09-03 13:09:20 -0400

    Finally have the initial boot-update-install-start sequence working for the build-server instance. Tested via postman locally good, but the test build-script.sh I have isn't producing the text file, likely a prob with way i'm running the command within exec function.

--- cicd/buildserver/scripts/build-script.sh
+++ cicd/buildserver/scripts/build-script.sh
@@ -3,9 +3,7 @@
 echo "---------- TESTING BUILD SCRIPT INIT ----------" >> ~/build-script.log
 echo "---------- BEGIN BUILD-SCRIPT.SH ----------" >> ~/build-script.log
 
-ls -la
 which gatsby >> ~/build-script.log
 gatsby --version >> ~/build-script.log
-
 
 echo "---------- END BUILD-SCRIPT.SH ----------" >> ~/build-script.log
--- cicd/terraform/scripts/main-startup.sh
+++ cicd/terraform/scripts/main-startup.sh
@@ -27,8 +27,8 @@ echo "<html><body><h1>Hello from Build Server</h1><p>Instance ID: $INSTANCE_ID</
 echo "---------- END MAIN-STARTUP SECTION ----------"
 echo "---------- BEGGINING AFTER STARTUP JOBS ----------"
 
-su ec2-user
-whoami
+# su ec2-user
+# whoami
 
 echo "---------- GIT CLONE OF ATO REPO ----------"
 
@@ -42,7 +42,8 @@ echo "---------- NPM INSTALL BUILDSERVER ----------"
 cd ~/abortoorbit.com/cicd/buildserver && npm install
 
 echo "---------- SET DEV-TEST.ENV AND CHMOD BUILD-SCRIPT ----------"
-echo "HOST=0.0.0.0\nPORT=8181\nNODE_ENV=development" > ~/abortoorbit.com/cicd/buildserver/.env
+echo "HOST=0.0.0.0" > ~/abortoorbit.com/cicd/buildserver/.env
+echo "PORT=8181" >> ~/abortoorbit.com/cicd/buildserver/.env
 cd ~/abortoorbit.com/cicd/buildserver/scripts && chmod u+x build-script.sh
 
 echo "---------- START SERVER ----------"
