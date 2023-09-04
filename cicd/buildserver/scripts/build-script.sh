be44841ca8b20d4fd15ded1382e124e5fa6d9c8b
Michael Stallings <stall84@gmail.com>
2023-09-04 11:43:57 -0400

    direct committing more script changes.

--- cicd/buildserver/scripts/build-script.sh
+++ cicd/buildserver/scripts/build-script.sh
@@ -3,7 +3,9 @@
 echo "---------- TESTING BUILD SCRIPT INIT ----------" >> ~/build-script.log
 echo "---------- BEGIN BUILD-SCRIPT.SH ----------" >> ~/build-script.log
 
-which gatsby >> ~/build-script.log
-gatsby --version >> ~/build-script.log
+# which gatsby >> ~/build-script.log
+# gatsby --version >> ~/build-script.log
+
+cd ~/aborttoorbit.com && npm run build
 
 echo "---------- END BUILD-SCRIPT.SH ----------" >> ~/build-script.log
