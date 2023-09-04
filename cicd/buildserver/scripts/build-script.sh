32b395e2961ed21f094ecf9770e85b230aacafad
Michael Stallings <stall84@gmail.com>
2023-09-04 12:26:25 -0400

    Trying to run an attempt at building remotely.

--- cicd/buildserver/scripts/build-script.sh
+++ cicd/buildserver/scripts/build-script.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+exec > >(tee /var/log/build-attempt.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 echo "---------- TESTING BUILD SCRIPT INIT ----------" >> ~/build-script.log
 echo "---------- BEGIN BUILD-SCRIPT.SH ----------" >> ~/build-script.log
@@ -6,6 +8,6 @@ echo "---------- BEGIN BUILD-SCRIPT.SH ----------" >> ~/build-script.log
 # which gatsby >> ~/build-script.log
 # gatsby --version >> ~/build-script.log
 
-cd ~/aborttoorbit.com && npm run build
+cd ~/aborttoorbit.com && npm run deploy:apply
 
 echo "---------- END BUILD-SCRIPT.SH ----------" >> ~/build-script.log
