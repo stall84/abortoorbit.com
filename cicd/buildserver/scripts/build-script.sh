93894e6d711b680e9f40d390773e4a7becfcbd2b
Michael Stallings <stall84@gmail.com>
2023-09-10 10:04:50 -0400

    still testing remote calls

--- cicd/buildserver/scripts/build-script.sh
+++ cicd/buildserver/scripts/build-script.sh
@@ -8,6 +8,7 @@ echo "---------- BEGIN BUILD-SCRIPT.SH ----------" >> ~/build-script.log
 # which gatsby >> ~/build-script.log
 # gatsby --version >> ~/build-script.log
 
-cd ~/aborttoorbit.com && npm run deploy:apply
+# cd ~/aborttoorbit.com && npm run deploy:apply
+cd /root/aborttoorbit.com && npm run remote:echo
 
 echo "---------- END BUILD-SCRIPT.SH ----------" >> ~/build-script.log
